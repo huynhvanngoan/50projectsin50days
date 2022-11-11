4de7a65784e59267bf9bb4b3cbf726f284073ece
Huynh Van Ngoan <huynhngoan389@gmail.com>
2022-11-11 22:20:42 +0700

    first commit

---
+++ .history/Day9/JS/script_20221111221735.js
@@ -0,0 +1,15 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+
+sounds.forEach (sound => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stop
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').appendChild(btn)
+})
